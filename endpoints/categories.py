f928e647182db389ae5d59897327d9d2e638595e
AdrianVilla1504 <adriancvila@gmail.com>
2024-12-15 18:49:36 -0500

    add categories refactor

--- endpoints/categories.py
+++ endpoints/categories.py
@@ -1,90 +1,86 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-import json
+from flask_restful import Resource, reqparse
 from utils.database_connection import DatabaseConnection
-
-def is_valid_token(token):
-    return token == 'abcd1234'
+from utils.authenticator import create_authenticator
 
 class CategoriesResource(Resource):
     def __init__(self):
-
+        # Instancia de conexión a la base de datos
         self.db = DatabaseConnection('db.json')
         self.db.connect()
-
+        
+        # Recuperación de datos de categorías
         self.categories_data = self.db.get_categories()
+
+        # Inicializamos el parser para manejar los parámetros de las solicitudes
         self.parser = reqparse.RequestParser()
 
     def get(self, category_id=None):
-        token = request.headers.get('Authorization')
-        if not token:
-            return { 'message': 'Unauthorized acces token not found'}, 401
-        if not is_valid_token(token):
-           return { 'message': 'Unauthorized invalid token'}, 401
-
+        """Obtiene las categorías."""
+        authenticator = create_authenticator()
+        auth_result = authenticator.authenticate()
+        if auth_result:
+            return auth_result
+        
+        # Buscar categoría específica si se proporciona un ID
         if category_id is not None:
-            category = next((p for p in self.categories_data if p['id'] == category_id), None)
-            if category is not None:
+            category = next((cat for cat in self.categories_data if cat['id'] == category_id), None)
+            if category:
                 return category
             else:
                 return {'message': 'Category not found'}, 404
-         
-        return self.categories_data 
+        
+        return self.categories_data
 
     def post(self):
-        token = request.headers.get('Authorization')
-        if not token:
-            return { 'message': 'Unauthorized acces token not found'}, 401
-        if not is_valid_token(token):
-           return { 'message': 'Unauthorized invalid token'}, 401
+        """Agrega una nueva categoría."""
+        authenticator = create_authenticator()
+        auth_result = authenticator.authenticate()
+        if auth_result:
+            return auth_result
 
+        # Definir los argumentos necesarios para la solicitud
         self.parser.add_argument('name', type=str, required=True, help='Name of the category')
- 
         args = self.parser.parse_args()
-        print("*****",args)
         new_category_name = args['name']
+
+        # Validar nombre de categoría
         if not new_category_name:
             return {'message': 'Category name is required'}, 400
 
-        categories = self.categories_data
-        if new_category_name in categories:
+        # Verificar si la categoría ya existe
+        if any(cat['name'] == new_category_name for cat in self.categories_data):
             return {'message': 'Category already exists'}, 400
-
-        new_category = {
-                'id': len(self.categories_data) + 1,
-                'name': new_category_name
-        }
-
-        categories.append(new_category)
-        self.categories_data = categories
         
+        # Crear una nueva categoría
+        new_category = {'id': len(self.categories_data) + 1, 'name': new_category_name}
+        self.categories_data.append(new_category)
         self.db.add_category(new_category)
 
         return {'message': 'Category added successfully'}, 201
 
     def delete(self):
-        token = request.headers.get('Authorization')
-        if not token:
-            return { 'message': 'Unauthorized acces token not found'}, 401
-        if not is_valid_token(token):
-           return { 'message': 'Unauthorized invalid token'}, 401
+        """Elimina una categoría."""
+        authenticator = create_authenticator()
+        auth_result = authenticator.authenticate()
+        if auth_result:
+            return auth_result
 
-        args = self.parser.parse_args()
+        # Definir los argumentos para la solicitud
         self.parser.add_argument('name', type=str, required=True, help='Name of the category')
         args = self.parser.parse_args()
         category_name = args['name']
- 
+
+        # Validar nombre de categoría
         if not category_name:
             return {'message': 'Category name is required'}, 400
 
+        # Buscar y eliminar la categoría
         category_to_remove = next((cat for cat in self.categories_data if cat["name"] == category_name), None)
-
-        if category_to_remove is None:
+        if not category_to_remove:
             return {'message': 'Category not found'}, 404
-        else:
-            categories = [cat for cat in self.categories_data if cat["name"] != category_to_remove]
-            self.categories_data = categories
-            self.db.remove_category(category_name)
 
-            return {'message': 'Category removed successfully'}, 200
+        # Eliminar categoría de la lista y la base de datos
+        self.categories_data = [cat for cat in self.categories_data if cat["name"] != category_name]
+        self.db.remove_category(category_name)
 
+        return {'message': 'Category removed successfully'}, 200
